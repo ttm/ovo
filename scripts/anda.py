7a426cee80eb78f9711c3620f80ac440f59fb093
Renato Fabbri <renato.fabbri@gmail.com>
2014-11-17 18:06:33 -0200

    adicionado script c turtle

---
+++ scripts/anda.py
@@ -0,0 +1,31 @@
+import turtle
+import random as r
+t=turtle.Turtle()
+t.color("red")
+t.forward(10)
+t.forward(100)
+t.right(100)
+
+for i in xrange(100):
+    t.right(10)
+    t.forward(20-i*.1)
+
+t=turtle.Turtle()
+t.color("green")
+
+for i in xrange(100):
+    t.right(10)
+    t.forward(20-i*.1)
+
+
+for i in xrange(100):
+    t.right(10)
+    t.forward(20-i*.1)
+
+
+
+for i in xrange(100):
+    t.right(10)
+    t.forward(20-i*.1)
+
+
